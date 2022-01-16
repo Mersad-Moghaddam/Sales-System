f4370e9e534c85110e25af198c77e04991a87f39
mersad <nickmersad81@gmail.com>
2022-01-16 13:48:38 +0330

    pytho

---
+++ learn.python/project1.py
@@ -0,0 +1,16 @@
+print('Find Minimum')
+num1=int(input('Enter Number1 : '))
+num2=int(input('Enter Number2 : '))
+num3=int(input('Enter Number3 : '))
+min=num1
+max=num1
+if num1>num2:
+   min=num2 
+else : 
+   max=num2
+if num2>num3 : 
+   min=num3   
+else : 
+   max=num3
+print('Min is : ',min)
+print('Max is : ',max)
